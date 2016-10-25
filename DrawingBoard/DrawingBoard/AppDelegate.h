0448a672fa212ea93f7e969dee86eb9101dd637b
ZhouYong <Tan@mac-mini.local>
2016-10-25 10:09:11 +0800

    画板

---
+++ DrawingBoard/DrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingBoard
+//
+//  Created by ZhouYong on 16/10/25.
+//  Copyright © 2016年 Rephontil/Yong Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingBoard/DrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingBoard
+//
+//  Created by ZhouYong on 16/10/25.
+//  Copyright © 2016年 Rephontil/Yong Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
